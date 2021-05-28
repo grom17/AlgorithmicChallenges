7d0b8fe84b91e01716fd108b3356208a80857b17
Roman Gomozov <rgomozov@oneinc.ru>
2021-05-28 17:44:51 +0700

    added HackerRank.WarmupChallenges.RepeatedString

---
+++ AlgorithmicChallenges.Tests/Problems/HackerRank/WarmupChallenges/RepeatedString/RepeatedStringTests.cs
@@ -0,0 +1,26 @@
+﻿namespace AlgorithmicChallenges.Tests.Problems.HackerRank.WarmupChallenges.RepeatedString
+{
+    using AlgorithmicChallenges.Problems.HackerRank.WarmupChallenges.RepeatedString;
+    using NUnit.Framework;
+    using static Helpers.StopwatchHelper;
+
+    [TestFixture]
+    public class RepeatedStringTests
+    {
+        [TestCase("abcac", 10, 4)]
+        [TestCase("aba", 10, 7)]
+        [TestCase("a", 1000000000000, 1000000000000)]
+        [TestCase("kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm", 736778906400, 51574523448)]
+        public void Test(string s, long n, long expectedResult)
+        {
+            // Arrange & Act
+            var (result, timeElapsed) = RunWithStopwatch(() => Solution.Run(s, n));
+
+            TestContext.Out.WriteLine($"Elapsed={timeElapsed}");
+            TestContext.Out.WriteLine($"Result={result}");
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ AlgorithmicChallenges/Problems/HackerRank/WarmupChallenges/RepeatedString/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace AlgorithmicChallenges.Problems.HackerRank.WarmupChallenges.RepeatedString
+{
+    using System.Linq;
+
+    /// <summary>https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup</summary>
+    public static class Solution
+    {
+        public static long Run(string s, long n)
+        {
+            static int CountA(string input)
+            {
+                return input.Count(x => x == 'a');
+            }
+            
+            var croppedPatternLength = (int)(n % s.Length);
+            var countAInCroppedPattern = CountA(s[..croppedPatternLength]);
+            return n / s.Length * CountA(s) + countAInCroppedPattern;
+        }
+    }
+}
