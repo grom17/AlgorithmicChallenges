4fca6314680c01356e911e74fe4c99df7a3b05cd
Roman Gomozov <rgomozov@oneinc.ru>
2021-05-28 19:20:31 +0700

    added HackerRank.InterviewPreparationKit.Array.TwoDimArrayDs

---
+++ AlgorithmicChallenges.Tests/Problems/HackerRank/InterviewPreparationKit/Array/TwoDimArrayDsTests.cs
@@ -0,0 +1,89 @@
+﻿namespace AlgorithmicChallenges.Tests.Problems.HackerRank.InterviewPreparationKit.Array
+{
+    using System.Collections.Generic;
+    using AlgorithmicChallenges.Problems.HackerRank.InterviewPreparationKit.Array;
+    using NUnit.Framework;
+    using static Helpers.StopwatchHelper;
+    
+    [TestFixture]
+    public class TwoDimArrayDsTests
+    {
+        [Test]
+        public void TestCase1()
+        {
+            // Arrange
+            var list = new List<List<int>>
+            {
+                new() { -9, -9, -9,  1, 1, 1 },
+                new() { -0, -9,  0,  4, 3, 2 },
+                new() { -9, -9, -9,  1, 2, 3 },
+                new() {  0,  0,  8,  6, 6, 0 },
+                new() {  0,  0,  0, -2, 0, 0 },
+                new() {  0,  0,  1,  2, 4, 0 }
+            };
+
+            var expectedResult = 28;
+            
+            // Act
+            var (result, timeElapsed) = RunWithStopwatch(() => Solution.Run(list));
+
+            TestContext.Out.WriteLine($"Elapsed={timeElapsed}");
+            TestContext.Out.WriteLine($"Result={result}");
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        [Test]
+        public void TestCase2()
+        {
+            // Arrange
+            var list = new List<List<int>>
+            {
+                new() { 1, 1, 1, 0, 0, 0 },
+                new() { 0, 1, 0, 0, 0, 0 },
+                new() { 1, 1, 1, 0, 0, 0 },
+                new() { 0, 0, 2, 4, 4, 0 },
+                new() { 0, 0, 0, 2, 0, 0 },
+                new() { 0, 0, 1, 2, 4, 0 }
+            };
+
+            var expectedResult = 19;
+            
+            // Act
+            var (result, timeElapsed) = RunWithStopwatch(() => Solution.Run(list));
+
+            TestContext.Out.WriteLine($"Elapsed={timeElapsed}");
+            TestContext.Out.WriteLine($"Result={result}");
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        [Test]
+        public void TestCase3()
+        {
+            // Arrange
+            var list = new List<List<int>>
+            {
+                new() { 1, 1,  1,  0,  0, 0 },
+                new() { 0, 1,  0,  0,  0, 0 },
+                new() { 1, 1,  1,  0,  0, 0 },
+                new() { 0, 9,  2, -4, -4, 0 },
+                new() { 0, 0,  0, -2,  0, 0 },
+                new() { 0, 0, -1, -2, -4, 0 }
+            };
+
+            var expectedResult = 13;
+            
+            // Act
+            var (result, timeElapsed) = RunWithStopwatch(() => Solution.Run(list));
+
+            TestContext.Out.WriteLine($"Elapsed={timeElapsed}");
+            TestContext.Out.WriteLine($"Result={result}");
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ AlgorithmicChallenges/Problems/HackerRank/InterviewPreparationKit/Array/TwoDimArrayDs.cs
@@ -0,0 +1,31 @@
+﻿namespace AlgorithmicChallenges.Problems.HackerRank.InterviewPreparationKit.Array
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays</summary>
+    public static class Solution
+    {
+        public static long Run(List<List<int>> arr)
+        {
+            var sums = new List<int>();
+
+            for (var r = 0; r < 4; r++)
+            {
+                for (var c = 0; c < 4; c++)
+                {
+                    sums.Add(GetHourglassSum(arr, r, c));
+                }
+            }
+            
+            return sums.Max();
+        }
+
+        private static int GetHourglassSum(List<List<int>> arr, int row, int column)
+        {
+            return arr[row][column] + arr[row][column + 1] + arr[row][column + 2] + 
+                   arr[++row][column + 1] + 
+                   arr[++row][column] + arr[row][column + 1] + arr[row][column + 2];
+        }
+    }
+}
